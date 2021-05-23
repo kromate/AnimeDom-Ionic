5bbda2357a3b6a015ab672c7d769336c7e5767a0
kromate <akpananthony33@gmail.com>
2021-05-23 20:46:12 +0100

    update

---
+++ src/components/genreCard.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="catCardCont">
+    <div v-for="(card, index) in cardsIcon" :key="index">
+      <label
+        :for="card.name"
+        @click="changeCard(card.name)"
+        :class="[selected == card.name ? 'select' : '', 'card']"
+      >
+        {{ card.name }}
+      </label>
+      <input type="radio" :id="card.name" :value="card.name" name="item" />
+    </div>
+
+    <!-- <div v-for="(card, index) in cardsImage" :key="index">
+      <label
+        :for="card.name"
+        @click="changeCard(card.name)"
+        :class="[selected == card.name ? 'select' : '', 'card']"
+      >
+        {{ card.name }}
+        <img :src="card.img" alt="" class="custom" v-if="loaded" />
+        <Loader w="32.02" h="50" b="8" v-else class="custom" />
+      </label>
+      <input type="radio" :id="card.name" :value="card.name" name="item" />
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "genreCard",
+  data() {
+    return {
+      inter: "",
+      loaded: false,
+      selected: "",
+    };
+  },
+  props: {
+    cardsIcon: {
+      type: Array,
+      required: true,
+    },
+  },
+  //   computed: {
+  //     selected() {
+  //       return this.$store.state.homeCategoryView;
+  //     },
+  //   },
+  methods: {
+    changeCard(data) {
+      this.selected = data;
+      //   this.$store.commit("changeHomeCategoryView", data);
+    },
+  },
+};
+</script>
+
+<style scooped>
+.select {
+  background: #794807d2 !important;
+  color: black !important;
+  font-weight: 600;
+}
+.catCardCont {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  flex-direction: column;
+}
+@media (max-width: 800px) {
+  .catCardCont {
+    flex-direction: row;
+  }
+}
+.card {
+  padding: 0px 0.8rem;
+  background-color: black;
+  color: #d79947;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 15px 7px;
+  border-radius: 7px;
+  width: 110px;
+  height: 50px;
+  cursor: pointer;
+}
+.card + input {
+  display: none;
+}
+label img {
+  height: 25px;
+  margin-left: 12px;
+}
+.custom {
+  position: relative;
+  right: -12px;
+  height: 48px;
+  border-radius: 4px;
+}
+</style>
--- src/views/Genre.vue
+++ src/views/Genre.vue
@@ -1,19 +1,19 @@
 <template>
   <IonContent class="mx-auto">
     <div class="flex bg-black mx-auto w-100 ">
-      <main class="flex-col mx-auto container" v-if="recentAnimeList.length">
+      <main class="flex-col mx-auto container" v-if="genreAnimeList.length">
         <div>
           <h1 class="green text-4xl font-bold mb-3">Genres</h1>
           <div class="flex flex-wrap justify-start ac">
-            <animeCard v-for="n in recentAnimeList" :data="n" :key="n" type="recent" />
+            <genreCard v-for="n in genreAnimeList" :cardsIcon="n" :key="n" type="recent" />
           </div>
         </div>
-        <div>
+        <!-- <div>
           <h1 class="green text-4xl font-bold mb-3">Popular</h1>
           <div class="flex flex-wrap justify-start ac">
-            <animeCard v-for="n in popularAnimeList" :data="n" :key="n" type="popular" />
+            <genreCard v-for="n in popularAnimeList" :data="n" :key="n" type="popular" />
           </div>
-        </div>
+        </div> -->
       </main>
 
       <div v-else class="mx-auto mt-5">
@@ -25,37 +25,24 @@
 
 <script>
 import { IonContent } from "@ionic/vue";
-import animeCard from "@/components/animeCard.vue";
+import genreCard from "@/components/genreCard.vue";
 import Loader from "@/components/Loader.vue";
 export default {
-  components: { animeCard, IonContent, Loader },
+  components: { genreCard, IonContent, Loader },
   data() {
     return {
       listening: false,
-      recentAnimeList: [],
+      genreAnimeList: [],
       popularAnimeList: [],
     };
   },
   methods: {
     init() {
-      fetch(`https://anime-web-scraper.herokuapp.com/recent/?page=1`)
+      fetch(`https://anime-web-scraper.herokuapp.com/genres/`)
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          this.recentAnimeList = data;
-          //   this.$store.commit("addsearchedRes", data);
-          //   this.loading = false;
-        })
-        .catch((err) => {
-          console.log(err);
-          alert("something went wrong");
-          this.loading = false;
-        });
-      fetch(`https://anime-web-scraper.herokuapp.com/popular/?page=1`)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          this.popularAnimeList = data;
+          this.genreAnimeList = data;
           //   this.$store.commit("addsearchedRes", data);
           //   this.loading = false;
         })
