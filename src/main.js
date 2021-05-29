ec0c782714f2e40669ab70337a3658e6e2dda6d4
kromate <akpananthony33@gmail.com>
2021-05-29 13:49:58 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/analytics";
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
@@ -8,18 +13,18 @@ import { IonicVue } from '@ionic/vue';
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
-// /* Basic CSS for apps built with Ionic */
-// import '@ionic/vue/css/normalize.css';
-// import '@ionic/vue/css/structure.css';
-// import '@ionic/vue/css/typography.css';
-
-// /* Optional CSS utils that can be commented out */
-// import '@ionic/vue/css/padding.css';
-// import '@ionic/vue/css/float-elements.css';
-// import '@ionic/vue/css/text-alignment.css';
-// import '@ionic/vue/css/text-transformation.css';
-// import '@ionic/vue/css/flex-utils.css';
-// import '@ionic/vue/css/display.css';
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGE_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASURE_ID
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 /* Theme variables */
 import './theme/variables.css';
