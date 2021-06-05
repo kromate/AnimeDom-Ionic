71132174685cfc90528f13342ad5d17cf3ba62f0
kromate <akpananthony33@gmail.com>
2021-06-05 01:24:50 +0100

    pwa update

---
+++ vue.config.js
@@ -0,0 +1,47 @@
+const SWPrecache = require('sw-precache-webpack-plugin')
+
+module.exports = {
+    // ...other vue-cli plugin options...
+  //   configureWebpack: config => {
+  //     if (process.env.NODE_ENV === 'production') {
+  //         config.plugins.push(
+  //             new SWPrecache({
+  //                 cacheId: 'nhb-year-in-pixels',
+  //                 filepath: 'dist/service-worker.js',
+  //                 staticFileGlobs: [
+  //                     'dist/*',
+  //                     'dist/**/*',
+  //                 ],
+  //                 stripPrefix: 'dist/',
+  //                 maximumFileSizeToCacheInBytes: 3097152,
+  //             })
+  //         )
+  //     }
+  // },
+    pwa: {
+      name: 'Animedom',
+      themeColor: '#000',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+      assetsVersion:'2',
+      manifestPath:'public/manifest.json',
+
+      // configure the workbox plugin
+      workboxPluginMode: 'InjectManifest',
+      workboxOptions: {
+        // swSrc is required in InjectManifest mode.
+        swSrc: './public/sw.js',
+        exclude: ['_redirects'],
+        // swDest: 'service-worker.js',
+        // ...other Workbox options...
+      }
+    },
+
+    chainWebpack: (config) => {
+
+      // A, remove the plugin
+      config.plugins.delete('prefetch')
+    }
+  }
+
